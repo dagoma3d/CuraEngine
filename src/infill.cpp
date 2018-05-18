f0d9b35ab725217165480bfa8fe23fb08158fa47
Tim Kuipers <t.kuipers@ultimaker.com>
2018-05-18 13:26:28 +0200

    Revert "Revert "fix: zigzaggify should put connections next to border, not on it""
    This reverts commit f74da6571469c23187f1f3296e4e86fa90e28ddc.
    
    The initial fix was the correct one, the other fix was wrong.

--- src/infill.cpp
+++ src/infill.cpp
@@ -80,6 +80,11 @@ void Infill::_generate(Polygons& result_polygons, Polygons& result_lines, const 
     if (in_outline.size() == 0) return;
     if (line_distance == 0) return;
 
+    if (zig_zaggify && (pattern == EFillMethod::LINES || pattern == EFillMethod::TRIANGLES || pattern == EFillMethod::GRID || pattern == EFillMethod::CUBIC || pattern == EFillMethod::TETRAHEDRAL || pattern == EFillMethod::QUARTER_CUBIC || pattern == EFillMethod::TRIHEXAGON))
+    {
+        outline_offset -= infill_line_width / 2; // the infill line zig zag connections must lie next to the border, not on it
+    }
+
     switch(pattern)
     {
     case EFillMethod::GRID:
