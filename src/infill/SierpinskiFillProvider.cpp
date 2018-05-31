3de066d52fba4903b09052ba1acc2f3aa9a26275
Tim Kuipers <t.kuipers@ultimaker.com>
2018-05-31 19:02:31 +0200

    feat: Cross3D now generates a continuous surface

--- src/infill/SierpinskiFillProvider.cpp
+++ src/infill/SierpinskiFillProvider.cpp
@@ -31,7 +31,7 @@ SierpinskiFillProvider::SierpinskiFillProvider(const AABB3D aabb_3d, coord_t min
 {
     subdivision_structure_3d->initialize();
     subdivision_structure_3d->createMinimalDensityPattern();
-    slice_walker_cross3d = subdivision_structure_3d->getBottomSequence();
+    slice_walker_cross3d = subdivision_structure_3d->getSequence(/* z = */ 0);
 }
 
 SierpinskiFillProvider::SierpinskiFillProvider(const AABB3D aabb_3d, coord_t min_line_distance, const coord_t line_width, std::string cross_subdisivion_spec_image_file, bool)
@@ -41,7 +41,7 @@ SierpinskiFillProvider::SierpinskiFillProvider(const AABB3D aabb_3d, coord_t min
 {
     subdivision_structure_3d->initialize();
     subdivision_structure_3d->createMinimalDensityPattern();
-    slice_walker_cross3d = subdivision_structure_3d->getBottomSequence();
+    slice_walker_cross3d = subdivision_structure_3d->getSequence(/* z = */ 0);
 }
 
 Polygon SierpinskiFillProvider::generate(EFillMethod pattern, coord_t z, coord_t line_width) const
@@ -61,7 +61,8 @@ Polygon SierpinskiFillProvider::generate(EFillMethod pattern, coord_t z, coord_t
     {
         assert(slice_walker_cross3d);
         subdivision_structure_3d->advanceSequence(*slice_walker_cross3d, z);
-        return subdivision_structure_3d->generateSierpinski(*slice_walker_cross3d);
+//         return subdivision_structure_3d->generateSierpinski(*slice_walker_cross3d);
+        return subdivision_structure_3d->generateCross(*slice_walker_cross3d, z);
     }
     else
     {
